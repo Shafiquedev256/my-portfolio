2502c4af46aa7b51192b04b4c06b57a76ef7ecf4
Musinguzi shafiq <shafiquedev256@gmail.com>
2024-01-30 11:06:50 +0300

    fixed bug in social links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,8 +10,8 @@ export const Header = ()=>{
         <div className="md:text-3xl font-bold text-2xl text-[#f1f1f1]">Shafiq</div>
         <nav className="space-x-4 flex flex-row items-center mr-2">
        <a href="https://github.com/Shafiquedev256?tab=overview&from=2024-01-01&to=2024-01-30"><img src={githubLogo}/></a>
-       <a href="https://www.linkedin.com/in/musinguzi-shafiq-ug?trk=contact-info"><img src={linkedinLogo}/></div>
-       <a href="shafiquedev256@gmail.com"><img src={gmailLogo}/></a>
+       <a href="https://www.linkedin.com/in/musinguzi-shafiq-ug?trk=contact-info"><img src={linkedinLogo}/></a>
+       <a href="mailto:shafiquedev256@gmail.com?subject=Hiring Musinguzi Shafiq wed dev"><img src={gmailLogo}/></a>
         </nav>
       </div>
     </header>
