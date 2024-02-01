293bd0901a42e04b029a68970031c48ed2eac947
Musinguzi shafiq <shafiquedev256@gmail.com>
2024-02-01 23:02:06 +0300

    changed theme

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -5,7 +5,7 @@ export const About = ()=>{
   return(
     <>
     <div className="font-light text-2xl ml-2 my-3">About Me</div>
-    <div className="text-gray-900 border-0 border-l-4 border-l-purple-800 pl-4 m-2 space-y-2">
+    <div className="border-gradient  text-gray-900 border-0 border-l-8  pl-4 m-2 space-y-2">
 <p>Hello! I'm <b>Shafiq</b>, a Web  developer. I developed an interest in programming while searching for a way to express who I am and what I believe in. My passion lies in crafting solutions that not only address personal challenges but also have a positive impact on the world. I strive to create avenues for everyone to access high-quality products and services from dedicated providers.</p>
 
 <p>Embarking on the MERN stack learning journey, I held a firm belief that the websites I build could transform the lives of like-minded individuals who share my beliefs.</p>
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,16 +1,22 @@
-
+import {useState} from "react"
 
 export const Projects = ()=>{
-  
+  const [openGame,setOpenGame] = useState(false)
   return (
     <> 
+    <div className="relative m-2"> 
+
       <div className="font-light text-2xl ml-2 my-3">Projects</div> 
       <div className="m-3">
-      <div className="font-bold border-0 border-b-[2px] border-b-purple-600 w-fit"> Game store </div> 
-      <div className="w-72 text-sm space-y-2 bg-gray-100 p-2 rounded my-2"> 
+      <div className="font-bold border-0 border-b-[2px] border-gradient w-fit" onClick={()=>{setOpenGame(!openGame)}}> Game store </div> 
+      {
+        openGame&&(  <div className="w-72 text-sm space-y-2 border-[2px] border-gradient  p-2   my-2"> 
       <p>
       I built a game store website fueled by my passion for video games. Using React, Tailwind CSS, Typescript, and Redux for the frontend, and Express.js, Node.js, and MongoDB for the backend (powered by the MERN stack), I crafted an API to seamlessly integrate games and trailers into the platform. This venture was inspired by my desire to make game buying simpler, leveraging my love for the MERN stack to create a tailored solution.</p> 
-     <a href="https://games-store-sandy.vercel.app/"> <div  className=" border-0 p-2  bg-blue-100 text-blue-900 font-bold text-sm rounded w-fit">live site</div></a>
+     <a href="https://games-store-sandy.vercel.app/"> <div  className=" border-0 p-2  bg-[#f6b7] text-white font-bold text-sm rounded w-fit">live site</div></a>
+      </div>)
+      }
+    
       </div>
       </div>
     </>
