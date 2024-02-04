f323b7498030c095e5d4f723228112e1bdba89a9
Musinguzi shafiq <shafiquedev256@gmail.com>
2024-02-04 15:46:49 +0300

    added new project

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,7 +1,9 @@
 import {useState} from "react"
 
 export const Projects = ()=>{
-  const [openGame,setOpenGame] = useState(false)
+  const [openGame,setOpenGame] = useState(false) 
+const [openGym,setOpenGym] = useState(false)  
+
   return (
     <> 
     <div className="relative m-2"> 
@@ -20,12 +22,13 @@ export const Projects = ()=>{
       </div> 
       
       <div className="m-3">
-      <div className="font-bold border-0 border-b-[2px] border-gradient w-fit" onClick={()=>{setOpenGame(!openGame)}}>Your Real Estate</div> 
+      <div className="font-bold border-0 border-b-[2px] border-gradient w-fit" onClick={()=>{setOpenGym(!openGym)}}>Gym Daddy</div> 
       {
-        openGame&&(  <div className="w-72 text-sm space-y-2 border-[2px] border-gradient  p-2   my-2"> 
+        openGym&&(  <div className="w-72 text-sm space-y-2 border-[2px] border-gradient  p-2   my-2"> 
       <p>
-      I built a game store website fueled by my passion for video games. Using React, Tailwind CSS, Typescript, and Redux for the frontend, and Express.js, Node.js, and MongoDB for the backend (powered by the MERN stack), I crafted an API to seamlessly integrate games and trailers into the platform. This venture was inspired by my desire to make game buying simpler, leveraging my love for the MERN stack to create a tailored solution.</p> 
-     <a href="https://games-store-sandy.vercel.app/"> <div  className=" border-0 p-2  bg-[#f6b7] text-white font-bold text-sm rounded w-fit">live site</div></a>
+      I created Gym Daddy with the goal of assisting individuals in meticulously planning their workouts before setting foot in the gym. Recognizing the challenges my father faced in tracking progress and rep increments during his gym sessions, I addressed this concern by employing a tech stack that includes React, Tailwind CSS, Redux, TypeScript, MongoDB, and Express. The platform is designed to cater to both mobile and desktop users, prioritizing a mobile-first approach for enhanced accessibility on smartphones.
+      </p> 
+     <a href="https://gym-daddy.vercel.app/"> <div  className=" border-0 p-2  bg-[#f6b7] text-white font-bold text-sm rounded w-fit">live site</div></a>
       </div>)
       }
       </div> 
