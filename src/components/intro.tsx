0b5d5e83259f081cc6c1b68fa857ffb08f3f6645
Musinguzi shafiq <shafiquedev256@gmail.com>
2024-03-16 10:02:28 +0300

    changed intro size

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -4,7 +4,7 @@ export const Intro = ()=>{
   
   return(
     <>
-    <div className="bg-img bg-center bg-cover w-screen h-[16rem] md:h-[25rem] font-sans flex flex-col justify-center items-center md:space-y-4">
+    <div className="bg-img bg-center bg-cover w-screen h-[16rem] md:h-[38rem] font-sans flex flex-col justify-center items-center md:space-y-4">
     <div className="text-6xl font-sans text-center py-2">Welcome!</div> 
     <div className="border-0 text-center p-3 w-[98%] text-gray-900 mx-2 md:text-2xl">
     <h1>I'm <b>Musinguzi Shafiq.</b></h1>
